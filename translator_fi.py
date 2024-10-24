37f33ee1b3d61d1f82515fcbc069dd3566320354
damao <jun.huang@eitei.co.jp>
2024-10-24 10:02:01 +0900

    translators

--- translator_fi.py
+++ translator_fi.py
@@ -3,7 +3,7 @@ from deep_translator import GoogleTranslator, exceptions
 
 # 定义输入和输出文件夹路径
 input_folder = 'Posts'  # 输入的 Markdown 文件夹
-output_folder = 'Posts_it'  # 输出的 Markdown 文件夹，指定为葡萄牙文
+output_folder = 'Posts_fi'  # 输出的 Markdown 文件夹，指定为葡萄牙文
 
 # 创建输出文件夹（如果不存在）
 os.makedirs(output_folder, exist_ok=True)
@@ -14,7 +14,7 @@ for filename in os.listdir(input_folder):
         input_file_path = os.path.join(input_folder, filename)  # 输入文件路径
 
         # 修改输出文件名，将后缀从 .jp.md 改为 .pt.md
-        output_file_name = filename.replace('.ja.md', '.it.md')  # 替换后缀名
+        output_file_name = filename.replace('.ja.md', '.fi.md')  # 替换后缀名
         output_file_path = os.path.join(output_folder, output_file_name)  # 输出文件路径
 
         # 读取 Markdown 文件内容
@@ -33,7 +33,7 @@ for filename in os.listdir(input_folder):
             newline_character = '\n' if line.endswith('\n') else ''
             trailing_spaces = len(line) - len(trimmed_line)  # 计算尾部空格数量
             try:
-                translated_line = GoogleTranslator(source='auto', target='italian').translate(trimmed_line)
+                translated_line = GoogleTranslator(source='auto', target='finnish').translate(trimmed_line)
                 if translated_line is None:
                     translated_line = trimmed_line  # 如果翻译返回 None，则使用原始行
 
@@ -51,7 +51,7 @@ for filename in os.listdir(input_folder):
             if line.startswith('title ='):
                 # 提取原有的标题并更新
                 original_title = line.split('"')[1]  # 提取引号内的原始标题
-                new_title = GoogleTranslator(source='auto', target='italian').translate(original_title)  # 翻译标题
+                new_title = GoogleTranslator(source='auto', target='finnish').translate(original_title)  # 翻译标题
                 final_content[i] = f'title = "{new_title}"\n'  # 替换为新的标题
 
         # 保存翻译后的内容到新文件
